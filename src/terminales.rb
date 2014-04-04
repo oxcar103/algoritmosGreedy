2f4761eddaf1a9dcef1241daa7fc490f4d3d5bcf
David Charte <fdavidcl@outlook.com>
2014-04-04 09:23:12 +0200

    Diccionarios

--- src/terminales.rb
+++ src/terminales.rb
@@ -2,13 +2,13 @@
 # encoding: utf-8
 
 def cambio (monedas, precio)
-  vuelta = []
+  vuelta = {}
 
   monedas.sort.reverse.each { |moneda|
     numero_monedas = precio / moneda
     precio = precio - numero_monedas*moneda
 
-    vuelta.push [moneda, numero_monedas]
+    vuelta[moneda] = numero_monedas
   }
 
   return vuelta
