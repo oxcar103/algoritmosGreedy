86116493c6b1df4c21f3323a54d72169da003e7c
M42 <mromang08@gmail.com>
2014-04-22 20:50:31 +0200

    Generación del grafo euclídeo. Ahora sí. Funcionando la generación del grafo simple a partir de una lista de puntos. No tiene muchos comentarios. Pero es más claro que todo lo demás.

--- src/euclidean_graph.py
+++ src/euclidean_graph.py
@@ -3,12 +3,40 @@
 
 # Siguiendo el ejemplo de https://github.com/israelst/Algorithms-Book--Python/blob/master/5-Greedy-algorithms/kruskal.py
 from sets import Set
+from math import sqrt
 
 # Formato de los grafos.
 empty_graph = {
     'vertices': [],
     'edges': set()
 }
+
+def euclideanGraph(points):
+    graph = {
+        'vertices': [],
+        'edges': set()
+    }
+
+    # Añade los nodos.
+    for p in points:
+        graph['vertices'].append(p)
+    
+    # Añade una arista de peso distancia entre cada dos puntos.
+    for i in range(len(points)): 
+        for j in range(i+1,len(points)):
+            p,q = points[i],points[j]
+            edge = (p,q,dist(p,q))
+            graph['edges'].add(edge)
+            
+    return graph
+
+
+def dist(p,q):
+    """ Devuelve distancia euclídea entre dos puntos."""
+    x1,y1 = p
+    x2,y2 = q
+    return sqrt((x1-x2)**2 + (y1-y2)**2)
+
 
 def kruskal(graph):
     # Árbol generador minimal.
