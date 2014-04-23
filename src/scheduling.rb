af339c4e570bcc3c8900c3da8b1e441c84c2f7c5
David Charte <fdavidcl@outlook.com>
2014-04-23 10:32:37 +0200

    Comentarios y ajustes scheduling

--- src/scheduling.rb
+++ src/scheduling.rb
@@ -3,12 +3,13 @@
 
 class Lesson < Array
     def initialize(s_hour,s_min,f_hour,f_min)
-        # Chanchullo para poder representar la hora de inicio y finalización
+        # Representación de la hora de inicio y finalización
         push Time.new(1970,1,1,s_hour,s_min)
         push Time.new(1970,1,1,f_hour,f_min)
     end
     
-    def solapa(otra)
+    # Detecta si la clase actual y otra se solapan
+    def solapa?(otra)
         (self[0] <= otra[0] && otra[0] <= self[1]) || (self[0] <= otra[1] && otra[1] <= self[1]) ||
         (self[0] >= otra[0] && otra[0] >= self[1]) || (self[0] >= otra[1] && otra[1] >= self[1])
     end
@@ -22,11 +23,13 @@ class Scheduling
     def planifica
         adyacentes = Array.new(@clases.size){Array.new(@clases.size)}
         
-        (0..@clases.size-1).each do |i|
-            # Hacemos 0 en la diagonal de la matriz de adyacencia...
-            adyacentes[i][i] = false       
-            (i+1..@clases.size-1).each do |j|
-                adyacentes[i][j] = adyacentes[j][i] = @clases[i].solapa @clases[j]
+        @clases.each_index do |i|
+            # Hacemos 0 en la diagonal de la matriz de adyacencia
+            adyacentes[i][i] = false
+
+            # En el grafo, dos clases son adyacentes si se solapan
+            (i+1 .. @clases.size-1).each do |j|
+                adyacentes[i][j] = adyacentes[j][i] = @clases[i].solapa? @clases[j]
             end
         end
         
@@ -40,13 +43,14 @@ class Scheduling
             actualizados = []
             
             sin_colorear.each do |v|
+                # Coloreamos la clase si no solapa con ninguna del color actual
                 if !(vertices[index].collect{ |u| adyacentes[v][u] }.member? true)
                     vertices[index] << v
                     actualizados << v
                 end
             end
             sin_colorear = sin_colorear - actualizados
-            index+=1
+            index += 1
         end
         
         vertices.map{ |i| i.map{ |j| @clases[j] } }       
