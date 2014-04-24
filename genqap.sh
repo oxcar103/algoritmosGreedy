3e01adce5f0a6638cb765518be5e973476ac104d
David Charte <fdavidcl@outlook.com>
2014-04-24 18:51:46 +0200

    Script tablas

---
+++ genqap.sh
@@ -0,0 +1,32 @@
+    TEXFILE="tex/table_qap2.tex"
+    touch $TEXFILE || exit -1
+
+    ALGORITHMS="inicial greedy-v1 greedy-v2 2-opt"
+    DATAFILES=`ls datos.qap/tai*`
+
+    echo -n "\\begin{center}
+    \\begin{longtabu} to \linewidth{ l | *4{d{0}}}  % máx 10 decimales
+\rowfont\bfseries Datos " > $TEXFILE
+
+    for alg in $ALGORITHMS; do
+        printf "& \multicolumn{1}{l}{$alg}" >> $TEXFILE
+    done
+
+    echo "\\\\ \\hline
+    \endhead
+    \endfoot
+    \\\\ \\hline
+    \endlastfoot" >> $TEXFILE
+
+    for file in $DATAFILES; do
+        echo -n "$file " >> $TEXFILE
+        RESULTS=`src/qap.rb $file | grep "Coste:" | cut -d" " -f2`
+        for res in $RESULTS; do
+            echo -n "& $res " >> $TEXFILE
+        done
+        echo "\\\\" >> $TEXFILE
+    done
+
+    echo "
+    \\end{longtabu}
+\\end{center}" >> $TEXFILE
